cc3a12a7e04bd222bf97c0314bac68c219e3cc15
edil3ra <edil3ra@gmail.com>
2022-07-23 18:03:16 +0200

    fix: prettier

--- types.tsx
+++ types.tsx
@@ -5,7 +5,7 @@
 
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import {
-   CompositeScreenProps,
+  CompositeScreenProps,
   NavigatorScreenParams,
 } from '@react-navigation/native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'

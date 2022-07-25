c56be05d1b2c3c2026068c5b7af111c1cd9bf30b
edil3ra <edil3ra@gmail.com>
2022-07-25 23:07:21 +0200

    clean: add eslint rules to warins on nested components

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@react-native-community', 'eslint-config-prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/react-no-unstable-nested-components': 'off',
+    'react/no-unstable-nested-components': ['off', { allowAsProps: true }],
     '@typescript-eslint/no-unused-vars': 'warn',
   },
 }

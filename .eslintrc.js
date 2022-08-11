44114e8c7f47fb97d69ea8680bd8736f95dace7b
edil3ra <edil3ra@gmail.com>
2022-08-11 23:44:37 +0200

    :wrench: allow inner style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@react-native-community', 'eslint-config-prettier'],
   rules: {
+    'react-native/no-inline-styles': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/no-unstable-nested-components': ['off', { allowAsProps: true }],
     '@typescript-eslint/no-unused-vars': 'warn',
